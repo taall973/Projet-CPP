802c20b2c7b453df5ccd51cfb69099e83b3c1fcd
turfa00 <80334127+turfa00@users.noreply.github.com>
2021-11-18 17:08:00 +0100

    Create trajet.hpp

---
+++ trajet.hpp
@@ -0,0 +1,21 @@
+#ifndef TRAJET_HPP
+#define TRAJET_HPP
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+class Passager;
+class Trajet{
+    //Je pense c'est mieux si on fait une classe des escales qui sera lie avec 
+    //les trajets comme ca, on fera une trajet d'une list static de tout les escales
+    //et une trajet prendra celles ou il passera slm
+    static vector<Trajet*>ToutEscales;
+    vector<Trajet*>escales;
+    string portArrive, portDepart;
+    vector<Passager*>LesPassagers;
+    //Date et Temps
+    public:
+};
+
+#endif

e82bc4ae8e203df7719af647ef2b881dfe060e9d
allantarcy <79646756+allantarcy@users.noreply.github.com>
2021-11-18 20:01:50 +0100

    update

--- trajet.hpp
+++ trajet.hpp
@@ -9,7 +9,6 @@ using namespace std;
 
 class Passager;
 class Trajet{
-    //test
     //Je pense c'est mieux si on fait une classe des escales qui sera lie avec 
     //les trajets comme ca, on fera une trajet d'une list static de tout les escales
     //et une trajet prendra celles ou il passera slm

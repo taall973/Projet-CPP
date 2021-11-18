d719c9c661797660c1331e7b9e63cabfc3f6f776
allantarcy <79646756+allantarcy@users.noreply.github.com>
2021-11-18 20:05:30 +0100

    update

--- trajet.hpp
+++ trajet.hpp
@@ -1,24 +1,25 @@
 #ifndef TRAJET_HPP
 #define TRAJET_HPP
 
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Passager;
-class Trajet{
-    //Je pense c'est mieux si on fait une classe des escales qui sera lie avec 
+class Trajet
+{
+    //Je pense c'est mieux si on fait une classe des escales qui sera lie avec
     //les trajets comme ca, on fera une trajet d'une list static de tout les escales
     //et une trajet prendra celles ou il passera slm
-    static vector<Trajet*>ToutEscales;
-    vector<Trajet*>escales;
+    static vector<Trajet *> ToutEscales;
+    vector<Trajet *> escales;
     string portArrive, portDepart;
-    vector<Passager*>LesPassagers;
+    vector<Passager *> LesPassagers;
     //Date et Temps
-    public:
-        Affiche();
+public:
+    void Affiche();
 };
 
 #endif

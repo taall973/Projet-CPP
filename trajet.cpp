6f1a6dd35c77da1628bfb2cc4f1d9e57c1f02a32
turfa00 <80334127+turfa00@users.noreply.github.com>
2021-11-21 22:34:52 +0100

    Update trajet.cpp

--- trajet.cpp
+++ trajet.cpp
@@ -1,4 +1,27 @@
 #include"trajet.hpp"
 #include"billet.hpp"
+#include"escales.hpp"
 
 using namespace std;
+
+Trajet::Trajet(string, string){
+
+}
+string Trajet::getPortDepart(){
+
+}
+string Trajet::getPortArrive(){
+
+}
+void Trajet::setPortDepart(string){
+
+}
+void Trajet::setPortArrive(string){
+
+}
+void Trajet::afficheEscales(){
+
+}
+void Trajet::affichePassagers(){
+    
+}

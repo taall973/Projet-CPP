6d6709e2699ee5f04ff29b95b9b8cbf38e8db0a9
turfa00 <80334127+turfa00@users.noreply.github.com>
2021-11-21 22:34:47 +0100

    Create escales.hpp

---
+++ escales.hpp
@@ -0,0 +1,23 @@
+#ifndef ESCALES_HPP
+#define ESCALES_HPP
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Trajet;
+class Escales{
+    int Id;
+    string Nom;
+    static vector <Escales*>ToutEscales;
+    public:
+        Escales(int, string);
+        int getId();
+        string getNom();
+        void setId(int);
+        void setNom(string);
+};
+
+#endif

4920b680c932e01744f80f73329eec94c1259c97
turfa00 <80334127+turfa00@users.noreply.github.com>
2021-11-23 14:20:44 +0100

    Update trajet.hpp

--- trajet.hpp
+++ trajet.hpp
@@ -5,12 +5,11 @@
 #include<string>
 #include<vector>
 
+using namespace std;
+
 class Passager;
 class Escales;
 class Trajet{
-    //Je pense c'est mieux si on fait une classe des escales qui sera lie avec 
-    //les trajets comme ca, on fera une trajet d'une list static de tout les escales
-    //et une trajet prendra celles ou il passera slm
     vector<Escales*>escales;
     string portDepart, portArrive;
     vector<Passager*>LesPassagers;

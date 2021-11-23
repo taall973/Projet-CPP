220b23ad5cdbcd886f9be90c5a488998b28da9e7
allantarcy <79646756+allantarcy@users.noreply.github.com>
2021-11-23 15:03:45 +0100

    Update navire.cpp

--- navire.cpp
+++ navire.cpp
@@ -4,47 +4,48 @@
 using namespace std;
         
 Navire::Navire(int i, string n){
-
+    Id = i;
+    Nom = n;
 }
 int Navire::getId(){
-
+    return Id;
 }
 string Navire::getNom(){
-
+    return Nom;
 }
 int Navire::getTonnage(){
-
+    return Tonnage;
 }
 int Navire::getCapaciteMarchandise(){
-
+    return CapaciteMarchandise;
 }
 int Navire::getMaxSeconds(){
-
+    return MaxSeconds;
 }
 int Navire::getMaxMatelots(){
-
+    return MaxMatelots;
 }
 Capitaine Navire::getCapitaine(){
-
+    return *c;
 }
-void Navire::setId(int){
-
+void Navire::setId(int i){
+    Id = i;
 }
-void Navire::setNom(string){
-
+void Navire::setNom(string n){
+    Nom = n;
 }
-void Navire::setTonnage(int){
-
+void Navire::setTonnage(int t){
+    Tonnage = t;
 }
-void Navire::setCapaciteMarchandise(int){
-
+void Navire::setCapaciteMarchandise(int c){
+    CapaciteMarchandise = c;
 }
-void Navire::setMaxSeconds(int){
-
+void Navire::setMaxSeconds(int ms){
+    MaxSeconds = ms;
 }
-void Navire::setMaxMatelots(int){
-
+void Navire::setMaxMatelots(int mm){
+    MaxMatelots = mm;
 }
-void Navire::setCapitaine(Capitaine *){
-    
+void Navire::setCapitaine(Capitaine * cap){
+    c = cap;
 }
